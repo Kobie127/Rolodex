c7e438cc654c687433fa4fe8422031b215dc0994
Kobie127 <arndtkobie@gmail.com>
2020-05-10 20:43:27 -0400

    Setup for searching characters in the rolodex

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => (
     <div className = 'card-container'>
         <img 
             alt = "monster" 
-            src={'https://robohash.org/${props.character.id}?set=set2&size=180x180'}
+            src={'https://robohash.org/${props.character.id}?set=set2&size=480x180'}
         />
         <h2>{props.character.name}</h2>
         <p>{props.character.email}</p>
